67ec81f9a7962eb1c1b06e39c996c1fc7bd96063
Vlad <59399760+Blinded-by-luck@users.noreply.github.com>
2020-09-23 13:12:24 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+
+client = socket.socket(
+
+    socket.AF_INET,
+    socket.SOCK_STREAM,
+
+)
+
+client.connect(
+    ("127.0.0.1", 1234)
+)
+
+while True:
+    data = client.recv(2048) #receive
+    print(data.decode("utf-8"))
