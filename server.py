eb535e34687db23ad131ee4b7de82e8c5124a4d3
Vlad <59399760+Blinded-by-luck@users.noreply.github.com>
2020-09-24 12:55:40 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import socket
 import threading
 
+address = "127.0.0.1"
 users = []
 server = socket.socket(
                     socket.AF_INET,
                     socket.SOCK_STREAM,
                 )
 server.bind(
-                    ("127.0.0.1", 1234) #localhost
+                    (address, 1234) #localhost
                 )
 server.listen(5)
 
@@ -15,9 +16,10 @@ server.listen(5)
 def start_server():
     while True:
         user_socket, address = server.accept()
-        print(f"User <{address[0]}> connected!")
+        print(f"Юзер <{address[0]}> присоединился!")
 
         users.append(user_socket)
+
         listen_accepted_user = threading.Thread(target=listen_user,
                      args=(user_socket, )
                      )
@@ -27,15 +29,15 @@ def start_server():
 
 def send_all(data):
     for user in users:
-        user.send(data)
+        user.send(data.encode("utf-8"))
 
 
 def listen_user(user):
-    print("Listening user")
     while True:
-        data = user.recv(2048)
-        print("User sent {}".format(data))
-        send_all(data)
+        data = user.recv(2048).decode("utf-8")
+        print("Юзер отправил {}".format(data))
+        send_all("Юзер внес изменение: "+data)
 
 if __name__ == '__main__':
+    print(f"Сервер запущен {address} запущен.")
     start_server()
