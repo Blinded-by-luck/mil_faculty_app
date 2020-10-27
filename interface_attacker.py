8306aca67b69be01985757d83846d8d353d3c0f5
Proskurin Valeriy <nub_2000@mail.ru>
2020-10-28 02:02:57 +0300

    Поправил, чтоб нападающий мог загрузить

--- interface_attacker.py
+++ interface_attacker.py
@@ -19,24 +19,24 @@ class Interface_attacker(QtWidgets.QMainWindow, design_attacker.Ui_interface_att
     def download_btn_click(self):
         # отлов исключений
         self.canvas.reset_temp_data()
-        options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getOpenFileName(self, "Открыть файл", "", "Special Files (*.mlbin)", options=options)
-        if file_name:
-            with open(file_name, 'rb') as file:
-                Node.reset_counter()
-                Arc.reset_counter()
-                self.canvas.net = pickle.load(file)
-                self.scene.clear()
-                for key_node in self.canvas.net.nodes:
-                    node = self.canvas.net.nodes[key_node]
-                    pixmap = self.canvas.get_appropriate_pixmap(node)
-                    custom_label = Custom_label(pixmap=pixmap, canvas=self.canvas, model_item=node)
-                    self.canvas.scene().addWidget(custom_label)
+        # options = QFileDialog.Options()
+        # file_name, _ = QFileDialog.getOpenFileName(self, "Открыть файл", "", "Special Files (*.mlbin)", options=options)
+        # if file_name:
+        with open('data.mlbin', 'rb') as file:
+            Node.reset_counter()
+            Arc.reset_counter()
+            self.canvas.net = pickle.load(file)
+            self.scene.clear()
+            for key_node in self.canvas.net.nodes:
+                node = self.canvas.net.nodes[key_node]
+                pixmap = self.canvas.get_appropriate_pixmap(node)
+                custom_label = Custom_label(pixmap=pixmap, canvas=self.canvas, model_item=node)
+                self.canvas.scene().addWidget(custom_label)
 
-                for key_arc in self.canvas.net.arcs:
-                    arc = self.canvas.net.arcs[key_arc]
-                    custom_line = Custom_line(canvas=self.canvas, model_item=arc,
-                                              x1=arc.node_from.x, y1=arc.node_from.y,
-                                              x2=arc.node_to.x, y2=arc.node_to.y)
-                    self.canvas.scene().addItem(custom_line)
+            for key_arc in self.canvas.net.arcs:
+                arc = self.canvas.net.arcs[key_arc]
+                custom_line = Custom_line(canvas=self.canvas, model_item=arc,
+                                          x1=arc.node_from.x, y1=arc.node_from.y,
+                                          x2=arc.node_to.x, y2=arc.node_to.y)
+                self.canvas.scene().addItem(custom_line)
 
